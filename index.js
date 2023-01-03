cd2fb5fa1084daf0cc8531c0e641557960966547
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-01-03 02:00:07 -0500

    added mongoose setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "expres"
+import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
@@ -10,29 +10,46 @@ import path from "path"
 import { fileURLToPath } from "url"
 
 // ********* Confiugurations **********
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
-// stored locally
-app.use("/assests", express.static(path.join(__dirname), 'public/assets'))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
 
-// ******** File Storage *********
+// ********* File Storage **********
+
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "public/assets")
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
 })
-// variable to upload files
 const upload = multer({ storage })
+
+// ********* Mongoose Setup **********
+
+const PORT = process.env.PORT || 6001
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch(error => console.log(`${error} cannot connect to server.`))
