44dd7a841dd7f6da035e8f77cf1fc221b55416c4
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-01-03 03:25:10 -0500

    added assets, controllers, client (install react)

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 import { register }  from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js"
 
 
 // ********* Confiugurations **********
@@ -39,9 +44,14 @@ const upload = multer({ storage })
 
 
 // ********* Routes w/ Files **********
-
 // 1st param: route to hit, 2nd param- middleware; uploads pic locally, 3rd param- controller
 app.post("/auth/register", upload.single("picture", register))
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
+
+//  Routes
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 
 // ********* Mongoose Setup **********
